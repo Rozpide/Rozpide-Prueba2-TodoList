03230ee1d6539a1143ec2a97801d4e59d4c0cb46
Rozpi <100348581+Rozpide@users.noreply.github.com>
2025-01-13 21:47:22 +0000

    consigo que la corra bien la funcionalidad :)

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -18,8 +18,12 @@ const TodoList = () => {
     const manejarTecla = (e) => {
         if (e.key === 'Enter'){
             añadirTareas();
-            console.log('se ejecuta bien')
+            
         }
+    }
+    const eliminarTarea = (index)=> {
+        const actualizarTareas = tareas.filter((_, i) => i !== index);
+        setTareas(actualizarTareas);
     }
 
 
@@ -27,6 +31,18 @@ const TodoList = () => {
         <>
             <h1>TodoList</h1>
             <input type="text" className="input-tarea" value={nuevaTarea} onChange={manejarEntrada} onKeyDown={manejarTecla} placeholder="añade tu tarea"/>
+            <ul className="lista-tareas">
+                {tareas.map((tarea, index) => (
+                    <li key={index} className={`tarea-item ${index > 0 ? 'tarea-apilada' : ''}`}>{tarea}
+                    <button onClick={() => eliminarTarea(index)} className="boton-eliminar oculto">
+						<span class="material-symbols-outlined">{/*añado la x de closed con google fonts, para que tenga los angulos iguales */}
+						close
+						</span>
+					</button>
+                    </li>
+                ))}
+                
+            </ul>
         </>
     )
 }
