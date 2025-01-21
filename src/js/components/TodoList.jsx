2da4efadc90a03f3fed7fe0a7daad73e6d56319d
Rozpi <100348581+Rozpide@users.noreply.github.com>
2025-01-21 15:24:04 +0000

    añado un contenedor que tenga el boton y le coloco estilo

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -99,9 +99,9 @@ const TodoList = () => {
     return (
         <>
             <h1 className="titulo">todos</h1>
-            <span>
+            <div className="contenedor-boton">
                 <button onClick={manejarClicDeEliminar} className="boton-eliminar-todas-tareas">Eliminar Todas las Tareas</button>
-            </span>
+            </div>
             <div className="entrada-tarea">
                 <input type="text" className="input-tarea" value={nuevaTarea} onChange={manejarEntrada} onKeyDown={manejarTecla} placeholder="What needs to be done?" />
                 
